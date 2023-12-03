4444842bd6ddd79d5e3de440977fd004a891a0c8
Prashant Kumar <prashantkumar69328@gmail.com>
2023-12-04 02:05:14 +0530

    React JS Code implementation

---
+++ Tut4/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+let name = "K";
+function App() {
+  return (
+    <>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact Us</li>
+    <div className="container">Hello WWE Champion {name} </div>
+    <div className="blank">
+      My APP</div>
+    </>
+    );
+}
+
+export default App;
